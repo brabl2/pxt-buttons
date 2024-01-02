8fcbf434ab40c41b1a6f61560fbc8a6c67fb1560
brabl2 <pavelvrbka@seznam.cz>
2024-01-02 21:20:54 +0100

    Added blocks description

--- buttons.cpp
+++ buttons.cpp
@@ -2,9 +2,15 @@
 
 using namespace pxt;
 
+//% color=#D400D4 weight=111 icon="\uf192" block="Buttons Ena/Dis"
 namespace buttons {
 
-//%
+/**
+* Enable the micro:bit buttons A+B (P5 and P11 can be used as GPIOs).
+*/
+//% blockId=buttons_ena_AB
+//% block="Enable buttons A+B"
+//% weight=20
 void enable() {
 #if MICROBIT_CODAL
     // V2 only
@@ -13,7 +19,12 @@ void enable() {
 #endif
 }
 
-//%
+/**
+* Disable the micro:bit buttons A+B (P5 and P11 can be used as GPIOs).
+*/
+//% blockId=buttons_dis_AB
+//% block="Disable buttons A+B"
+//% weight=20
 void disable() {
 #if MICROBIT_CODAL
     // V2 only
--- buttons.ts
+++ buttons.ts
@@ -1,14 +1,22 @@
+
+//% color=#D400D4 weight=111 icon="\uf192" block="Buttons Ena/Dis"
 namespace buttons {
 
     /**
-    * Enable the micro:bit buttons. 
+    * Enable the micro:bit buttons A+B (P5 and P11 can be used as GPIOs).
     */
+    //% blockId=buttons_ena_AB
+    //% block="Enable buttons A+B"
+    //% weight=20
     //% shim=buttons::enable
     export function enable(): void { basic.pause(100) }
 
     /**
-    * Disable the micro:bit buttons. 
+    * Disable the micro:bit buttons A+B (P5 and P11 can be used as GPIOs).
     */
+    //% blockId=buttons_dis_AB
+    //% block="Disable buttons A+B"
+    //% weight=20
     //% shim=buttons::disable
     export function disable(): void { basic.pause(100) }
 
