54241f1a2716ac30586082dd46ad8ab810bf49fc
brabl2 <pavelvrbka@seznam.cz>
2024-01-04 22:48:18 +0100

    cleanup

--- buttons.ts
+++ buttons.ts
@@ -6,7 +6,7 @@ namespace buttons {
     * Disable the micro:bit(V2) button A (P5 can be used as GPIO).
     */
     //% blockId=buttons_dis_A
-    //% block="Disable button A"
+    //% block="disable button A"
     //% weight=29
     //% shim=buttons::disableA
     export function disableA(): void { basic.pause(100) }
@@ -15,16 +15,16 @@ namespace buttons {
     * Disable the micro:bit(V2) button B (P11 can be used as GPIO).
     */
     //% blockId=buttons_dis_B
-    //% block="Disable button B"
+    //% block="disable button B"
     //% weight=28
     //% shim=buttons::disableB
     export function disableB(): void { basic.pause(100) }
 
     /**
-    * Disable the micro:bit(V2) buttons A+B (P5 and P11 can be used as GPIOs).
+    * Disable the micro:bit(V2) buttons A and B (P5 and P11 can be used as GPIOs).
     */
     //% blockId=buttons_dis_AB
-    //% block="Disable buttons A+B"
+    //% block="disable buttons A and B"
     //% weight=27
     //% shim=buttons::disable
     export function disable(): void { basic.pause(100) }
@@ -33,7 +33,7 @@ namespace buttons {
     * Enable the micro:bit(V2) button A (P5 can be used as GPIO).
     */
     //% blockId=buttons_ena_A
-    //% block="Enable button A"
+    //% block="enable button A"
     //% weight=19
     //% shim=buttons::enableA
     export function enableA(): void { basic.pause(100) }
@@ -42,16 +42,16 @@ namespace buttons {
     * Enable the micro:bit(V2) button B (P11 can be used as GPIO).
     */
     //% blockId=buttons_ena_B
-    //% block="Enable button B"
+    //% block="enable button B"
     //% weight=18
     //% shim=buttons::enableB
     export function enableB(): void { basic.pause(100) }
 
     /**
-    * Enable the micro:bit(V2) buttons A+B (P5 and P11 can be used as GPIOs).
+    * Enable the micro:bit(V2) buttons A and B (P5 and P11 can be used as GPIOs).
     */
     //% blockId=buttons_ena_AB
-    //% block="Enable buttons A+B"
+    //% block="enable buttons A and B"
     //% weight=17
     //% shim=buttons::enable
     export function enable(): void { basic.pause(100) }
